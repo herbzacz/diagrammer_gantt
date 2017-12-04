0f00e2832561a4fd8b22e28225f7ff278f31249c
Herbert_Zaczyk <Herbert_Zaczyk@DESKTOP-G96PM3I>
2017-12-04 20:39:04 +0100

    first

---
+++ Gantt Chart.R/Script.R
@@ -0,0 +1,13 @@
+library(DiagrammeR)
+
+mermaid(
+"gantt
+dateFormat  DD-MM-YYYY
+title Zeitplan
+
+section A section
+Aufgabe 1            :done,    des1, 03-12-2017,07-12-2017
+Aufgabe 2            :active,  des2, 08-12-2017, 3d
+Aufgabe 3           :         des3, after des2, 5d
+Aufgabe 4           :         des4, after des3, 5d
+")
